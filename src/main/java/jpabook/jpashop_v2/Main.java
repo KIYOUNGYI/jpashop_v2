c9fd8c73870543e47b73936f15d6d128e380db77
yky1990@glowmee.com <yky1990@glowmee.com>
2020-06-01 17:59:39 +0900

    n+1 문제 확인

--- src/main/java/jpabook/jpashop_v2/Main.java
+++ src/main/java/jpabook/jpashop_v2/Main.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop_v2;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
@@ -10,6 +12,14 @@ public class Main {
 	{
 		SpringApplication app = new SpringApplication(Main.class);
 		app.run(args);
+	}
+
+	// 대충 이런게 있어, 이걸 쓸 필요가 없는거임
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+		hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5Module;
 	}
 
 }
